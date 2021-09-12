c5d63b66a4a9560b2084d2b9fdfaa5ba1ab5a036
fys <fys@example.com>
2021-09-12 22:03:52 +0800

    1

--- spring4_day01/src/main/java/cn/itcast/spring/a_quickstart/IUserService.java
+++ spring4_day01/src/main/java/cn/itcast/spring/a_quickstart/IUserService.java
@@ -2,5 +2,6 @@ package cn.itcast.spring.a_quickstart;
 
 public interface IUserService {
 	//登录
+	//git注释
 	public void login();
 }
--- spring4_day01/src/main/java/cn/itcast/spring/a_quickstart/UserDaoImpl.java
+++ spring4_day01/src/main/java/cn/itcast/spring/a_quickstart/UserDaoImpl.java
@@ -1,8 +1,10 @@
 package cn.itcast.spring.a_quickstart;
 
 public class UserDaoImpl implements IUserDao {
+	//git注释
 	@Override
 	public void findByUsernameAndPassword() {
 		System.out.println("UserDaoImpl-dao层被调用了");
 	}
+
 }
