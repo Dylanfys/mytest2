ccc5923afe63e087a30d82b9b107f803cddf557e
fys <fys@example.com>
2021-09-12 22:06:01 +0800

    2

--- spring4_day01/src/main/java/cn/itcast/spring/a_quickstart/UserDaoImpl.java
+++ spring4_day01/src/main/java/cn/itcast/spring/a_quickstart/UserDaoImpl.java
@@ -1,6 +1,7 @@
 package cn.itcast.spring.a_quickstart;
 
 public class UserDaoImpl implements IUserDao {
+	//aa
 	@Override
 	public void findByUsernameAndPassword() {
 		System.out.println("UserDaoImpl-dao层被调用了");
